d1b8e8790951a0328c13ebde966d2da3d6df52fe
hari <harii1996@gmail.com>
2024-11-06 16:42:03 +0530

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/baking-studio">
+  <BrowserRouter>
     <StoreContextProvider>
        <App />
       </StoreContextProvider>
