d1df6c15216155e6c32cf23ab63242f846fa1896
hari <harii1996@gmail.com>
2024-11-06 16:32:43 +0530

    second commit

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-
-export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://hariiharan1411:opensesame@cluster0.mzmci.mongodb.net/baking-studio')
-    .then(()=>{
-        console.log('Connected to DB');
-    })
-}
